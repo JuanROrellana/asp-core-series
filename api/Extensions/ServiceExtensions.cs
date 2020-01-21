314fc5acb1fecf8dfb65d435c792bc3bd0db853a
Juan Ramirez <ramirez.orellana.juanjose@gmail.com>
2020-01-21 01:43:46 -0600

    Add Extensions mEthods

--- api/Extensions/ServiceExtensions.cs
+++ api/Extensions/ServiceExtensions.cs
@@ -1,6 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace api.Extensions
 {
     public static class ServiceExtensions
     {
+        
+        //Configure Cors to Allow external client to connect to the api
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", 
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+        }
+
+        //For Deployment
+        public static void ConfigureIisIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options => 
+            {
+ 
+            }); 
+        }
     }
 }
