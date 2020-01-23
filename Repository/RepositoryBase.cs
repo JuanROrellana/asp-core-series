be93ea5d7dbcd69edf59a61044bb848b7ddd24ef
Juan Ramirez <ramirez.orellana.juanjose@gmail.com>
2020-01-22 23:54:18 -0600

    Update to abstract class repositoty

--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -8,7 +8,7 @@ using System.Linq.Expressions;
 
 namespace Repository
 {
-    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
     {
         protected RepositoryContext RepositoryContext { get; set; }
         
